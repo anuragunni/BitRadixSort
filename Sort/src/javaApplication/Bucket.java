8fc319ed178879505d1d532bbc7179b64eb9c908
AnuragUnnikrishnan <anurag.unnikrishnan@gmail.com>
2016-07-18 03:53:13 +0530

    First working draft

---
+++ Sort/src/javaApplication/Bucket.java
@@ -0,0 +1,14 @@
+package javaApplication;
+import java.util.*;
+public class Bucket {
+
+    public List<Integer> arr;
+   // private int count;
+    public Bucket()
+          {
+              arr=new ArrayList<Integer>();
+           //   count=0;
+          }
+   
+    
+}
